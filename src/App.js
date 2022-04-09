9936d671937ab8365f65dca8e7d2678df948d4e0
Haliaa <halyna.shevchyk.pm.2020@lpnu.ua>
2022-04-09 20:11:37 +0300

    master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Actor from "./components/Actor";
+import Users from "./components/Users";
+
+const actors = [
+    {id:1, name: 'Tom', img: 'https://i.pinimg.com/originals/68/a6/7c/68a67c62366eaff0fa686f81d18ed4ea.png'},
+    {id:2, name: 'Jerry', img: 'https://i.pinimg.com/564x/a1/66/75/a1667537bfd23e4d72a3c826e2b8db8d.jpg'},
+    {id:3, name: 'Luntik', img: 'https://i.pinimg.com/564x/72/62/c2/7262c2118c72347a08db8cce57527957.jpg'},
+    {id:4, name: 'Minion', img: 'https://i.pinimg.com/564x/cb/c5/fd/cbc5fd14edc9546da00ee7961f59a086.jpg'}
+]
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <>
+          {/*{actors.map(actor=><Actor key={actor.id} name1={actor.name} img1={actor.img}/>)}*/}
+          <Users/>
+      </>
+  )
 }
 
 export default App;
+
+
+
+
+
