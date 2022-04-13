7d5991f6cdeb36b29cfcb89bfb90e81d70b89e5c
Haliaa <halyna.shevchyk.pm.2020@lpnu.ua>
2022-04-13 14:18:30 +0300

    homework

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
+// Вивести всіх юзерів, біля кожного юзера кнопка GetDetails.
+// При натисканні на кнопку праворуч повинна з’явитися повністю вся інформація про юзера і кнопка при натисканні на яку знизу повинні з’явитися його пости
+// Структура схематично зображена вище...
+
+
+
+
 import './App.css'
-import Actor from "./components/Actor";
 import Users from "./components/Users";
 
-const actors = [
-    {id:1, name: 'Tom', img: 'https://i.pinimg.com/originals/68/a6/7c/68a67c62366eaff0fa686f81d18ed4ea.png'},
-    {id:2, name: 'Jerry', img: 'https://i.pinimg.com/564x/a1/66/75/a1667537bfd23e4d72a3c826e2b8db8d.jpg'},
-    {id:3, name: 'Luntik', img: 'https://i.pinimg.com/564x/72/62/c2/7262c2118c72347a08db8cce57527957.jpg'},
-    {id:4, name: 'Minion', img: 'https://i.pinimg.com/564x/cb/c5/fd/cbc5fd14edc9546da00ee7961f59a086.jpg'}
-]
 
 
 function App() {
