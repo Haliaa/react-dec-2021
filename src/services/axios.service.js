439a51091718019188baee6a9341bd50a40d5305
Haliaa <halyna.shevchyk.pm.2020@lpnu.ua>
2022-04-15 12:49:57 +0300

    homework

--- src/services/axios.service.js
+++ src/services/axios.service.js
@@ -2,3 +2,6 @@ import axios from "axios";
 const axiosService = process.env.REACT_APP_API
 
 export {axiosService}
+import baseURL from "../constants/urls";
+
+export const axiosService = axios.create({ baseURL });
