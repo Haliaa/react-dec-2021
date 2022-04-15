9ee68e8599ad6d54dcadc19e7084493f1614a9ba
Haliaa <halyna.shevchyk.pm.2020@lpnu.ua>
2022-04-15 12:46:56 +0300

    homework

--- src/App.js
+++ src/App.js
@@ -2,20 +2,14 @@
 // При натисканні на кнопку праворуч повинна з’явитися повністю вся інформація про юзера і кнопка при натисканні на яку знизу повинні з’явитися його пости
 // Структура схематично зображена вище...
 
-
-
-
 import './App.css'
 import Users from "./components/Users";
 
-
-
 function App() {
   return (
-      <>
-          {/*{actors.map(actor=><Actor key={actor.id} name1={actor.name} img1={actor.img}/>)}*/}
+      <div>
           <Users/>
-      </>
+      </div>
   )
 }
 
---
+++ src/services/axios.service.js
@@ -0,0 +1,4 @@
+import axios from "axios";
+const axiosService = process.env.REACT_APP_API
+
+export {axiosService}
