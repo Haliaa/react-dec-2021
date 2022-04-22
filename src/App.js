96ad862e18072ec9bb47710b4631c5e9f03879f8
Haliaa <halyna.shevchyk.pm.2020@lpnu.ua>
2022-04-22 18:15:33 +0300

    lection

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-const App = () => {
-    return (
-        <div>
+import {ClassComponent, FunctionComponent} from "./components";
+import {useState} from "react";
 
+const App = () => {
+ const [flag, setFlag]= useState(true)
+    return (
+        // <div>        {/*Class*/}
+        //     <div>{flag && <ClassComponent ssss={'MyProps'}/>}</div>
+        //     <button onClick={()=>setFlag(prev=>!prev)}>Hide</button>
+        // </div>
+        <div>       {/*Function*/}
+            <div>{flag && <FunctionComponent ssss={'MyProps'}/>}</div>
+            <button onClick={()=>setFlag(prev=>!prev)}>Hide</button>
         </div>
     );
 };
---
+++ src/components/ClassComponent.js
@@ -0,0 +1,50 @@
+import {Component} from "react";
+
+class ClassComponent extends Component{
+    constructor(props) {
+        super(props);
+        this.state={
+            a:0,
+            b:25
+        }
+        console.log('constructor')
+    }
+
+    componentDidMount() {
+        console.log('componentDidMount')
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        if(prevState.a ===5){return 'Snapshot!!!!'}
+        return null
+    }
+
+    componentWillUnmount() {
+        console.log('componentWillUnmount') //спрацьовує, коли вся компонента пропадає з html(div)
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('componentDidUpdate')
+        console.log('prevProps',prevProps)
+        console.log('prevState',prevState)
+        console.log('snapshot',snapshot)
+    }
+
+    inc(){
+        this.setState(prev=>({a:prev.a + 1}))
+    }
+    render() {
+        console.log('render')
+        return(
+            <div>
+                Hello from class
+                <div>{this.props.ssss}</div>
+                <div>A:{this.state.a}</div>
+                <div>B:{this.state.b}</div>
+                <button onClick={()=>this.inc()}>Inc</button>
+            </div>
+        )
+    }
+}
+
+export {ClassComponent}
