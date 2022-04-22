b1d4d4f65ad79a7f9afb2e6073c42a84f1b41f37
Haliaa <halyna.shevchyk.pm.2020@lpnu.ua>
2022-04-22 18:15:33 +0300

    lection

---
+++ src/components/FunctionComponent.js
@@ -0,0 +1,30 @@
+import {useEffect, useState} from "react";
+
+const FunctionComponent = (props) => {
+    const {ssss} = props
+    console.log('constructor')
+    const [state, setState] = useState({a:0, b:25})
+    const inc = () => {
+      setState({...state, a: state.a + 1})
+    }
+    useEffect(()=>{
+        console.log('componentDidMount')
+        return ()=>{
+            console.log('componentWillMount')
+        }
+    },[])
+    useEffect(()=>{
+        console.log('componentDidUpdate')
+    },[props, state])
+    return (
+        <div>
+            {console.log('render')}
+            Hello from function
+            <div>{ssss}</div>
+            <div>A:{state.a}</div>
+            <div>B:{state.b}</div>
+            <button onClick={()=>inc()}>Inc</button>        </div>
+    );
+};
+
+export {FunctionComponent};
