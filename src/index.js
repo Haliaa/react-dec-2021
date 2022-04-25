a5d763784cec9e95578af28a8cca07718f7f7359
Haliaa <halyna.shevchyk.pm.2020@lpnu.ua>
2022-04-25 22:38:25 +0300

    redux_base_1

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+
+
+function reducer (state=0, action) {
+    // console.log(action)
+    switch (action.type){
+        case 'INC':
+            return state + action.payload
+        case 'DEC':
+            return state - action.payload
+        case 'RESET':
+            return state= action.payload
+    }
+}
+let store = createStore(reducer)
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App/>)
+root.render(<Provider store={store}><App/></Provider>)
 
 
 
